ae35c543a3c9478bbbb1665d8f6d91399a441b89
Maksim Semenov <maksimuma@list.ru>
2023-06-20 19:06:30 +0300

    feat: добавить компонент NotFound

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+import './NotFound.css'
+
+function NotFound() {
+  return (
+    <section className='not-found'>
+      <div className='not-found__container'>
+        <h1 className='not-found__title'>404</h1>
+        <p className='not-found__subtitle'>Страница не найдена</p>
+      </div>
+      <Link className='not-found__link' to='/'>Назад</Link>
+    </section>
+  )
+}
+
+export default NotFound;
