4d0ef1ac8845028fbbb907326582de34a4a6dc84
Maksim Semenov <maksimuma@list.ru>
2023-06-20 18:44:06 +0300

    feat: добавить компоненты авторизации и регистрации

---
+++ src/components/Login/Login.js
@@ -0,0 +1,32 @@
+import AuthForm from '../AuthForm/AuthForm';
+import './Login.css'
+
+function Login() {
+  return (
+    <AuthForm
+      title='Рады видеть!'
+      btnName='Войти'
+      help='Ещё не зарегистрированы?'
+      link='Регистрация'
+    >
+      <fieldset className='auth-form__fieldset'>
+        <label className='auth-form__field'>
+          E-mail
+          <input
+            className='auth-form__input'
+          />
+          <span className='auth-form__error'></span>
+        </label>
+        <label className='auth-form__field'>
+          Пароль
+          <input
+            className='auth-form__input'
+          />
+          <span className='auth-form__error'></span>
+        </label>
+      </fieldset>
+    </AuthForm>
+  )
+}
+
+export default Login;
