4cb6bfd7ec3eaea09645e50778668872f1f12ae9
Maksim Semenov <maksimuma@list.ru>
2023-06-28 17:35:09 +0300

    feat: добавить фильтр по длительности

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -1,7 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './FilterCheckbox.css'
+import { useLocation } from 'react-router-dom';
 
 function FilterCheckbox({ onChecked }) {
+  const [isShortMovie, setIsShortMovie] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/movies') {
+      const isShortMovieHistory = localStorage.getItem('isShortMovie');
+      if (isShortMovieHistory) {
+        setIsShortMovie(JSON.parse(isShortMovieHistory));
+      }
+    }
+  }, []);
+
+  const handleIsShortMovie = (e) => {
+    setIsShortMovie(e.target.checked);
+    onChecked(e.target.checked);
+  }
+
   return (
     <div className='filter-checkbox'>
       <label className='filter-checkbox__label'>
@@ -9,6 +28,8 @@ function FilterCheckbox({ onChecked }) {
           className='filter-checkbox__switch'
           type='checkbox'
           name='isShortMovie'
+          onChange={handleIsShortMovie}
+          checked={isShortMovie ?? false}
         />
         <span className='filter-checkbox__slider'></span>
       </label>
